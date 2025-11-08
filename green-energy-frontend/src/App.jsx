2cf78507fafca2587f84784fd91a89e22691bbfe
arez-sajeel <arezsajeel@outlook.com>
2025-11-08 12:52:46 +0000

    feat sprint1.4:RegisterPage

--- green-energy-frontend/src/App.jsx
+++ green-energy-frontend/src/App.jsx
@@ -2,6 +2,7 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login.jsx";
 import Register from "./pages/Register.jsx";
+import HomeOwner from "./pages/HomeOwner.jsx";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Route path="/" element={<Navigate to="/login" replace />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/homeowner" element={<HomeOwner />} />
     </Routes>
   );
 }
