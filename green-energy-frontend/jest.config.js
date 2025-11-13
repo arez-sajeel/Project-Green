3fea3e153624932bc3fd0e39c91f12600836b56a
arez-sajeel <arezsajeel@outlook.com>
2025-11-13 15:29:11 +0000

    Revert "Added Jest config, test files, and updated frontend code"
    This reverts commit bdf3cd5dfe324144ef574c66857c7764fab497a2.

--- green-energy-frontend/jest.config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  testEnvironment: "jsdom",
-  transform: {
-    "^.+\\.(js|jsx)$": "babel-jest",
-  },
-  moduleFileExtensions: ["js", "jsx"],
-  setupFilesAfterEnv: ["<rootDir>/setupTests.js"],  // MUST match path
-};
